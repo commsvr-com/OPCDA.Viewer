7dfd2cf4eb3ff0f0b5a46124030b5de9dc6c33f7
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-08-03 11:58:33 +0000

    CommServer.DA.DataPorter - update - OPC Viewer updated. Issue #: 4849

--- VS/trunk/CommServer.DA.Viewer/Viewer.UserInterface/Properties/AssemblyInfo.cs
+++ VS/trunk/CommServer.DA.Viewer/Viewer.UserInterface/Properties/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿//_______________________________________________________________
-//  Title   :  Assembly info for: CAS.CommServer.DAViewerAddressSpace
+//  Title   :  Assembly info for: CAS.CommServer.DA.Viewer.UserInterface
 //  System  : Microsoft VisualStudio 2013 / C#
-//  $LastChangedDate$
-//  $Rev$
-//  $LastChangedBy$
-//  $URL$
-//  $Id$
+//  $LastChangedDate:  $
+//  $Rev: $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
 //
 //  Copyright (c) 2000-2016 CAS LODZ POLAND
 //  +48 (42) 686 25 47
@@ -17,18 +17,17 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CAS.CommServer.DAViewerAddressSpace")]
-[assembly: AssemblyDescription("Dictionary Address Space Description - Provides portable schema to port address space and provide dictionary services")]
+[assembly: AssemblyTitle("CAS.CommServer.DA.Viewer.UserInterface")]
+[assembly: AssemblyDescription("OPC Viewer - CAS OPC Data Access Client")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CAS")]
 [assembly: AssemblyProduct("CAS CommServer Family")]
 [assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
 [assembly: AssemblyTrademark("CommServer")]
 [assembly: AssemblyCulture("")]
-[assembly: Guid("e3bdcac3-296e-48f8-8999-616c7cc6afa4")]
+[assembly: Guid("338E5EA2-C45F-4b9c-B86C-0353813D1680")]
 [assembly: AssemblyVersion("3.10.03.*")]
 [assembly: AssemblyFileVersion("3.10.03")]
-
 [assembly: InternalsVisibleToAttribute("<dll name without extension>, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b3cff1bec656a6" +
                                                                        "e3784c75cacce8ee3bc695d0ce135d820c86465eb49b2353f0438d28ad9a458e5350c0c141393d" +
                                                                        "d9ad606bd9f4e0a8e1b0c49fa37835630b16798ece56df81c756f4c6bd37f07d68cc91ebfa7be7" +
