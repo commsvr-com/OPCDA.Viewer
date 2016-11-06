81bd2e142d6e09ea22057e14cd4d3c2c5ee4bc81
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-11-06 21:40:40 +0000

    OPC VIewer Installing license has failed - working on the CommServer.DA.Viewer  - release 3.11.17 nuget package  - refactoring externals only unimportant changes. Issue #: 4884

--- VS/trunk/CommServer.DA.Viewer/Viewer.Scripts/CreateTag.cmd
+++ VS/trunk/CommServer.DA.Viewer/Viewer.Scripts/CreateTag.cmd
@@ -6,8 +6,8 @@ rem//  $URL$
 rem//  $Id$
 
 
-svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Viewer.rel_3_11_16  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Viewer.rel_3_11_16"
-svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.DA.Viewer/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Viewer.rel_3_11_16 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Viewer.rel_3_11_16"
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Viewer.rel_3_11_17  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Viewer.rel_3_11_17"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CommServer.DA.Viewer/ svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Viewer.rel_3_11_17 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CommServer.DA.Viewer.rel_3_11_17"
 
 pause ....
 
